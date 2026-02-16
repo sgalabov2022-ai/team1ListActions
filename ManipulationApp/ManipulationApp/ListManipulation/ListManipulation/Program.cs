ab51588567d7a02ee58f934094bea7602c0e3698
Stefan <s.galabov2022@pmg-pernik.com>
2026-02-16 08:17:09 +0200

    addd VS

---
+++ ManipulationApp/ManipulationApp/ListManipulation/ListManipulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] cmd = Console.ReadLine().Split().ToArray();          
+
+            switch (cmd[0])
+            {
+                case "add":
+                    break;
+                case "ins":
+                    break;
+                case "contains":                    
+                    break;
+                case "delete":
+                    break;
+                case "remove":
+                    break;
+                case "print":
+                    break;
+                case "countOdds": 
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
